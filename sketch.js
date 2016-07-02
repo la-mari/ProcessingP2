7a3dc062c3522b05a534c9def120e55198a1a9b1
la-mari <karima19@gmail.com>
2016-07-02 13:43:37 -0700

    added option 1 and 2 and switching function

--- sketch.js
+++ sketch.js
@@ -1,5 +1,6 @@
 console.log("js page linked");
 
+var option = 1;
 function setup() {
 	createCanvas(windowWidth, windowHeight);
 	noFill();
@@ -15,17 +16,36 @@ function draw(){
 	// 	stroke(i/width * 255); 
 	// 	line(i, 0, i, height);
 	// }
-	var count = 0;
-	for (var x=50; x<=width-50; x+=50){
-		for (var y=50; y<=height-50; y+=50){
-			//vanishing point
-			strokeWeight(count*0.02);
-			line(x, y, width/2, height/2);
-			count++;
 
-			//gird of X's
-			// line(x-5, y-5, x+5, y+5);
-			// line(x+5, y-5, x-5, y+5);
+	if(option==1){
+		for (var x=50; x<=width-50; x+=50){
+			for (var y=50; y<=height-50; y+=50){
+		//grid of X's
+			line(x-5, y-5, x+5, y+5);
+			line(x+5, y-5, x-5, y+5);
+			}
 		}
+	}
+
+	else if(option==2){
+		// var count = 0;
+		for (var x=50; x<=width-50; x+=50){
+			for (var y=50; y<=height-50; y+=50){
+				//vanishing point
+				// strokeWeight(count*0.02);
+				line(x, y, width/2, height/2);
+				// count++;
+			}
+		}
+
+	}
+
+}
+
+function mousePressed(){
+	console.log(option);
+	option++;
+	if(option > 6){
+		option =1;
 	}
 }
