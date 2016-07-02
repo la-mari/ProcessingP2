c7c0d8cc88fceb897951f3ec38043bc9de525138
la-mari <karima19@gmail.com>
2016-07-02 14:59:52 -0700

    added 4th sketch

--- sketch.js
+++ sketch.js
@@ -46,6 +46,17 @@ function draw(){
 			}
 		}
 	}
+
+	else if(option==4){
+		for (var x=50; x<=width-50; x+=50){
+			for (var y=50; y<=height-50; y+=50){
+				for (var i=0; i<16; i+=4){
+					line(x+i, y, x+i, y+12);
+				}
+				line(x, y, x+12, y+12);
+			}
+		}
+	}
 }
 
 function mousePressed(){
